33d7b71cadcabc7353140d395be3c56a71b8a9a0
Can Kolay <cankolay3499@gmail.com>
2022-05-14 23:35:59 +0300

    chore: remove box component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo: React.FC<{}> = () => {
   return (
     <>
-      <h1 className="text-3xl font-medium">{process.env.NEXT_PUBLIC_APP_NAME}</h1>
+      <h1 className="text-3xl font-bold text-primary-500">{process.env.NEXT_PUBLIC_APP_NAME}</h1>
     </>
   )
 }
--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,3 @@
 export * from '@/components/Container'
 export * from '@/components/Header'
 export * from '@/components/Logo'
-export * from '@/components/Box'
