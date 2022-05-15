9d2230790242e1672a5829f2064acdaae0f5dac5
Can Kolay <cankolay3499@gmail.com>
2022-05-15 13:55:04 +0300

    chore: use swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,13 @@ import type { AppProps } from 'next/app'
 
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { SWRConfig } from 'swr'
+import axios from 'axios'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const appName: string = process.env.NEXT_PUBLIC_APP_NAME
+
+  const fetcher: any = (url: string) => axios.get(url).then((res) => res.data)
 
   return (
     <>
@@ -21,18 +25,20 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
         />
       </Head>
 
-      <motion.main transition={{ duration: 1 }} animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-        <Header />
-        <motion.main
-          transition={{ duration: 0.5 }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          key={Math.random()}
-        >
-          <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <motion.main transition={{ duration: 1 }} animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+          <Header />
+          <motion.main
+            transition={{ duration: 0.5 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={Math.random()}
+          >
+            <Component {...pageProps} />
+          </motion.main>
         </motion.main>
-      </motion.main>
+      </SWRConfig>
     </>
   )
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Container } from '@/components'
-import axios from 'axios'
 import Link from 'next/link'
-
-import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import useSWR from 'swr'
+import { AnimatePresence, motion } from 'framer-motion'
 
-type ProjectsPageProps = {
-  repos: any
-}
-
-const ProjectsPage: React.FC<ProjectsPageProps> = ({ repos }: ProjectsPageProps) => {
+const ProjectsPage: React.FC<{}> = () => {
   const appName: string = process.env.NEXT_PUBLIC_APP_NAME
+
+  const { data: repos }: any = useSWR('https://api.github.com/users/CanKolay3499/repos')
 
   return (
     <>
@@ -21,43 +18,59 @@ const ProjectsPage: React.FC<ProjectsPageProps> = ({ repos }: ProjectsPageProps)
 
       <Container>
         <h1 className="text-4xl font-bold mb-6 mt-2">Projects</h1>
-        {repos.map((repo: any, index: number) => {
-          return (
-            <div
-              className="mb-4 flex flex-col justify-center bg-secondary py-2 px-4 rounded-2xl"
-              key={index}
-            >
-              <Link href={repo.html_url} passHref>
-                <a>
-                  <h2 className="text-3xl items-center flex font-medium text-primary-400">
-                    {repo.name}
-                  </h2>
-                </a>
-              </Link>
-              {repo.fork && <span className="font-medium my-2 text-secondary-disabled">Fork</span>}
-              {repo.description && (
-                <p className="text-secondary-disabled text-sm font-bold font-mono">
-                  {repo.description}
-                </p>
-              )}
-            </div>
-          )
-        })}
+        <AnimatePresence exitBeforeEnter>
+          {!repos ? (
+            <>
+              <motion.div
+                transition={{ duration: 0.5 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="mb-4 flex flex-col animate-pulse justify-center bg-secondary py-2 px-4 rounded-2xl"
+              >
+                <div className="flex-1">
+                  <div className="text-3xl mb-3 bg-tertiary h-3 rounded shadow w-9/12 block font-medium text-primary-400"></div>
+                  <div className="text-3xl mb-1 bg-tertiary h-3 rounded shadow w-full block font-medium text-primary-400"></div>
+                  <div className="text-3xl bg-tertiary h-3 rounded shadow w-full block font-medium text-primary-400"></div>
+                </div>
+              </motion.div>
+            </>
+          ) : (
+            <>
+              {repos.map((repo: any, index: number) => {
+                return (
+                  <motion.div
+                    transition={{ duration: 0.5 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="mb-4 flex flex-col justify-center bg-secondary py-2 px-4 rounded-2xl"
+                    key={index}
+                  >
+                    <Link href={repo.html_url} passHref>
+                      <a>
+                        <h2 className="text-3xl items-center flex font-medium text-primary-400">
+                          {repo.name}
+                        </h2>
+                      </a>
+                    </Link>
+                    {repo.fork && (
+                      <span className="font-medium my-2 text-secondary-disabled">Fork</span>
+                    )}
+                    {repo.description && (
+                      <p className="text-secondary-disabled text-sm font-bold font-mono">
+                        {repo.description}
+                      </p>
+                    )}
+                  </motion.div>
+                )
+              })}
+            </>
+          )}
+        </AnimatePresence>
       </Container>
     </>
   )
 }
 
-const getServerSideProps: GetServerSideProps = async () => {
-  const res = await axios({ method: 'GET', url: 'https://api.github.com/users/CanKolay3499/repos' })
-  const repos = res.data
-
-  return {
-    props: {
-      repos
-    }
-  }
-}
-
-export { getServerSideProps }
 export default ProjectsPage
