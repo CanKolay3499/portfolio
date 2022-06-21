ad9c3ffce2614b6757ad45c9968dc927c335c035
Can Kolay <cankolay3499@gmail.com>
2022-06-21 18:56:49 +0300

    chore: small improvements

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Logo: React.FC<{}> = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold text-primary-500">{process.env.NEXT_PUBLIC_APP_NAME}</h1>
+      <h1 className="text-3xl font-bold text-primary-500 dark:text-primary-400">
+        {process.env.NEXT_PUBLIC_APP_NAME}
+      </h1>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,13 @@ const HomePage: React.FC<{}> = () => {
         <div className="mb-4">
           <h2 className="text-3xl font-medium mb-1">Short bio</h2>
           <div>
-            <div className="text-secondary-disabled font-mono">
-              Merhaba, My name is <span className="font-bold text-primary-500 text-lg">Can</span>. I
-              living in Turkey 🇹🇷 and I&apos;m 13 years old
+            <div className="text-secondary font-mono">
+              Merhaba, My name is <span className="font-bold text-primary-400 text-lg">Can</span>. I
+              living in Turkey 🇹🇷 and I&apos;m 13 years o
             </div>
+            <span className="text-secondary-disabled text-xs font-medium">
+              * Merhaba means hi in english.
+            </span>
           </div>
         </div>
 
@@ -58,7 +61,7 @@ const HomePage: React.FC<{}> = () => {
               <h3 className="font-bold text-xl">Backend</h3>
               <div className="font-medium font-mono text-secondary flex flex-col">
                 <span>Node.js</span>
-                <span>Nestjs</span>
+                <span>Nest.js</span>
               </div>
             </div>
 
@@ -80,7 +83,7 @@ const HomePage: React.FC<{}> = () => {
               <div>
                 <p className="flex items-center">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary">I started learning lua</p>
+                  <p className="text-secondary">Started learning lua</p>
                 </p>
               </div>
             </div>
@@ -90,12 +93,12 @@ const HomePage: React.FC<{}> = () => {
               <div>
                 <p className="flex">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary items-center">I started learning React</p>
+                  <p className="text-secondary items-center">Started learning React & Vue</p>
                 </p>
 
                 <p className="flex">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary items-center">I started learning Tailwindcss</p>
+                  <p className="text-secondary items-center">Started learning Tailwindcss & Scss</p>
                 </p>
               </div>
             </div>
@@ -105,12 +108,12 @@ const HomePage: React.FC<{}> = () => {
               <div>
                 <p className="flex">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary items-center">I started learning Node.js</p>
+                  <p className="text-secondary items-center">Started learning Node.js</p>
                 </p>
 
                 <p className="flex">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary items-center">I started learning Python</p>
+                  <p className="text-secondary items-center">Started learning Python</p>
                 </p>
               </div>
             </div>
@@ -124,7 +127,7 @@ const HomePage: React.FC<{}> = () => {
                 </p>
                 <p className="flex">
                   <span className="text-primary-600 mr-2">-</span>
-                  <p className="text-secondary items-center">My programming journey has started</p>
+                  <p className="text-secondary items-center">My programming journey started.</p>
                 </p>
               </div>
             </div>
