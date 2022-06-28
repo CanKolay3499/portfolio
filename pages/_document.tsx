c6d26c525ee69e25a31586bcd983b4f50a8c00d0
Can Kolay <cankolay3499@gmail.com>
2022-06-28 15:44:03 +0300

    chore: remove unnecessary pwa tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,23 +10,14 @@ const Document: React.FC<{}> = () => {
           rel="stylesheet"
         />
 
-        <link rel="manifest" href="/manifest.json" />
-
         <link rel="shortcut icon" href="/favicon.ico" />
 
-        <meta name="application-name" content="CanKolay3499" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="CanKolay3499" />
         <meta name="description" content="Portfolio - CanKolay3499" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#FFFFFF" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="CanKolay3499" />
         <meta property="og:description" content="Portfolio - CanKolay3499" />
-        <meta property="og:site_name" content="CanKolay3499" />
       </Head>
       <body className="text-primary bg-primary">
         <Main />
