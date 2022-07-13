208d163cd513e8aa27a990175cb27a378490b843
Can Kolay <cankolay3499@gmail.com>
2022-07-13 14:09:59 +0300

    fix: add `NEXT_PUBLIC_SITE_URL` env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
 }
 
